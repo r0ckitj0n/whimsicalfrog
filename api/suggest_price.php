3903583734f685087df0ce54925dd9d5acb95562
Jon Graves <jon@catn8.us>
2025-06-22 10:12:16 -0400

    Auto-commit before deployment

--- api/suggest_price.php
+++ api/suggest_price.php
@@ -25,7 +25,7 @@ if ($isLoggedIn) {
         $userData = json_decode($userData, true);
     }
     if (is_array($userData)) {
-        $isAdmin = isset($userData['role']) && $userData['role'] === 'Admin';
+        $isAdmin = isset($userData['role']) && strtolower($userData['role']) === 'admin';
     }
 }
 
